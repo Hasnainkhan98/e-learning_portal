b6fe5daa09345b1e232614e0cbbf87aa6f91519d
Hasnain Khan <108027426+Hasnainkhan98@users.noreply.github.com>
2022-06-23 19:55:28 +0530

    Update urls.py

--- teaching_blog/app_users/urls.py
+++ teaching_blog/app_users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from app_users import views
+from .views import RegisterAPI
+from knox import views as knox_views
+from .views import LoginAPI
 
 urlpatterns= [
 	path('',views.HomeView.as_view(),name='index'),
@@ -7,4 +10,8 @@ urlpatterns= [
     path('user_login/', views.user_login, name='user_login'),
     path('logout/', views.user_logout, name='user_logout'),
     path('contact/', views.ContactView.as_view(), name="contact"),
+	path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
